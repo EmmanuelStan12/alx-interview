1b965f5d569b3f8253c4e54b91f52c847f302365
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-03 22:25:11 +0100

    task: Pascal triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n):
+    """
+    Returns a list of lists representing Pascal's triangle up to the nth row.
+    """
+    if n == 0:
+        return []
+
+    result = [[1]]
+    
+    for i in range(1, n):
+        prev_row = result[-1]
+        cur_row = [1]
+        
+        for j in range(1, len(prev_row)):
+            cur_row.append(prev_row[j - 1] + prev_row[j])
+        
+        cur_row.append(1)
+        result.append(cur_row)
+
+    return result
