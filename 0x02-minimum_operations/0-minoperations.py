44c45a70b682514745392e0a6673eaf939727b5b
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-28 08:23:56 +0100

    task: Minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/env/python3
+"""Solving minimum operations
+"""
+
+
+def minOperations(n: int) -> int:
+    """Calculate no of operations
+    """
+    if n == 1:
+        return 0
+
+    dp = [0] * (n + 1)
+    for num in range(2, n + 1):
+        dp[num] = num
+        
+        for factor in range(1, num // 2 + 1):
+            if num % factor == 0:
+                dp[num] = min(dp[num], dp[factor] + (num // factor))
+
+    return dp[n]
