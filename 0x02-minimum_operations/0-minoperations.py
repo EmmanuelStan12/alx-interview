b18cc318c5bc47bf85bf402e894408c0f7cc6800
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-28 13:36:25 +0100

    fix: Memory optimizations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,14 +6,16 @@
 def minOperations(n: int) -> int:
     """Calculate no of operations
     """
-    if n == 1:
+    if n <= 1:
         return 0
 
-    dp = [0] * (n + 1)
-    for num in range(2, n + 1):
-        dp[num] = num
-        for factor in range(1, num // 2 + 1):
-            if num % factor == 0:
-                dp[num] = min(dp[num], dp[factor] + (num // factor))
+    operations = 0
+    divisor = 2
+    
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
 
-    return dp[n]
+    return operations
