9b6710f93cbc1f6595fb5f831c16c03dce87cc4e
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-11-21 20:52:43 +0100

    task: Rotate 3d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Rotate 2d matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotate 2d matrix
+    """
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1, n):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+    for row in matrix:
+        row.reverse()
+
+
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,1 @@
+task: Rotate 2d matrix
