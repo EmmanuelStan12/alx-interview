7bbe4f9018726405a532cd9f6379431936446923
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-11-28 19:12:31 +0100

    fix: Test cases

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,6 +7,8 @@ from typing import List
 def makeChange(coins: List[int], total: int) -> int:
     """Fewest no of coins to get to total
     """
+    if total <= 0:
+        return 0
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
