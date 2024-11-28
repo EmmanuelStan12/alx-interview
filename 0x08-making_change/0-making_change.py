3fe7ce54dff4526c393b09a20114c4ecee062b32
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-11-28 18:38:57 +0100

    fix: Checker

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,17 +7,11 @@ from typing import List
 def makeChange(coins: List[int], total: int) -> int:
     """Fewest no of coins to get to total
     """
-    n = len(coins)
-    dp = [[float('inf')] * (total + 1) for _ in range(n + 1)]
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
 
-    for i in range(n + 1):
-        dp[i][0] = 0
+    for coin in coins:
+        for j in range(coin, total + 1):
+            dp[j] = min(dp[j], 1 + dp[j - coin])
 
-    for i in range(1, n + 1):
-        for j in range(total + 1):
-            dp[i][j] = dp[i - 1][j]
-
-            if j >= coins[i - 1]:
-                dp[i][j] = min(dp[i][j], 1 + dp[i][j - coins[i - 1]])
-
-    return dp[n][total] if dp[n][total] != float('inf') else -1
+    return dp[total] if dp[total] != float('inf') else -1
